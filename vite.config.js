669a58fbef68def3b8006f7074dd055bdc380da0
Mylle Braga <mcsouza297@gmail.com>
2025-09-03 14:04:01 -0300

    Configura projeto para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Esta linha é essencial e deve estar exatamente assim:
   base: '/trilha-programacao/',
 })
